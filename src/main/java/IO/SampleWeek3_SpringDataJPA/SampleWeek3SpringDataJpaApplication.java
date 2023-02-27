cef430a7fe14fefd81348152eb24eea0d5ac2e24
021Skyfall <021skyfall@gmail.com>
2023-02-27 09:01:29 +0900

    데이터 액세스 계층 구현

---
+++ src/main/java/IO/SampleWeek3_SpringDataJPA/SampleWeek3SpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package IO.SampleWeek3_SpringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWeek3SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWeek3SpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IO/SampleWeek3_SpringDataJPA/SampleWeek3SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package IO.SampleWeek3_SpringDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWeek3SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
